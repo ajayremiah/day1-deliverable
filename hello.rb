f7684a918dd4dcc009e52077816406d5ba6bf564
ajayremiah <avtalaue@up.edu.ph>
2026-05-12 15:34:02 +0800

    Updated Ruby script

--- hello.rb
+++ hello.rb
@@ -1,1 +1,7 @@
-puts "Day 1: Environment successfully set up."
+puts "Day 1 [May 12, 2026]: Environment successfully set up. 
+
+Today, I set up the environment by installing RVM, Ruby, and GIT. 
+Since I already have a GitHub account, I created a repository on GitHub named day1-deliverable.
+I also complpeted lessons from the Foundations course on The Odin Project, which covered how the internet works and the basics of Web Development.
+
+I'm excited to continue learning in the coming days."
